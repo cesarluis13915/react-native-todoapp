379c9fdd39dffac999bcbe5fc739ddc6826efaf2
cesarluis <cesarluis13915@gmail.com>
2018-09-27 20:03:57 -0600

    Working on TextInput component. Doesn't work yet.

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import Header from "./Header";
-import Footer from "./Footer";
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Header from './Header';
+import Footer from './Footer';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+      items: []
+    };
+
+    this.handleAddItem = this.handleAddItem.bind(this);
+  }
+
+  handleAddItem() {
+    if (!this.state.value) return;
+
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ];
+
+    this.setState({
+      items: newItems,
+      value: ''
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header />
+        <Header
+          value={this.state.value}
+          onAddItem={this.handleAddItem}
+          onChange={(value) => this.setState({ value })}
+        />
         <View style={styles.content}>
         </View>
         <Footer />
@@ -19,7 +52,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F5F5F5"
+    backgroundColor: '#F5F5F5'
   },
   content: {
     flex: 1
